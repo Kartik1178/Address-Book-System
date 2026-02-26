c5d148f26bb4887184c365cdf1550e5867ccfe1b
Kartikeya Pusapati <swagboss600@gmail.com>
2026-02-26 07:32:18 +0530

    [Kartikeya]: UC1 Create Contact class with fields

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,5 +1,19 @@
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        Contact person = new Contact(
+                "Kartikeya",
+                "Pusapati",
+                "Ambattur",
+                "Chennai",
+                "Tamil Nadu",
+                "600053",
+                "9876543210",
+                "kartikeya@email.com"
+        );
+
+        person.displayContact();
     }
 }
